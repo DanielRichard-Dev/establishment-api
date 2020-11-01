75ae7bd0800cf3ba001676547a4c66954413daec
Daniel Richard <daniel.richard@starcorp.com.br>
2020-11-01 19:21:41 -0300

    add cors.

--- establishment-api/Startup.cs
+++ establishment-api/Startup.cs
@@ -61,6 +61,8 @@ namespace establishment_api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
